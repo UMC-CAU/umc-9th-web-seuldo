d0a2dcd8e1e531459689c4f2e811d502df60e707
anjaein <wodls9840@gmail.com>
2025-10-04 18:48:24 +0900

    페이지네이션 기능 구현

--- mission2/src/pages/movies.tsx
+++ mission2/src/pages/movies.tsx
@@ -3,11 +3,13 @@ import type { Movie, MovieResponse } from '../types/movie';
 import { LoadingIcon } from "../components/loading-icon";
 import axios from 'axios';
 import { ErrorText } from '../components/error';
+import { PageNavigator } from '../components/pagination';
 
 export default function MoviesPage(){
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(1);
 
   console.log(import.meta.env.VITE_TMDB_KEY);
 
@@ -16,7 +18,7 @@ export default function MoviesPage(){
         try{
         setIsLoading(true)
         const { data } = await axios.get<MovieResponse>(
-          'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+          `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`,
           {
             headers: {
               Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -31,7 +33,7 @@ export default function MoviesPage(){
         }
       }
       fetchMovies()
-  }, []);
+  }, [page]);
 
   if (isLoading){
     return <LoadingIcon />;
@@ -41,22 +43,25 @@ export default function MoviesPage(){
   }
 
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-6 gap-2">
-        {movies.map((movie) => 
-        <div className='relative group overflow-hidden'>
-          <img key={movie.id}
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            className="rounded"
-          />
-          <div className='absolute inset-0 flex flex-col justify-center items-center backdrop-blur-md text-white
-                        opacity-0 group-hover:opacity-100 duration-300 rounded'>
-            <h2 className="font-bold text-lg leading-snug">{movie.title}</h2>
-            <p className='text-sm line-clamp-5'>{movie.overview}</p>
+    <>
+      <PageNavigator page={page} setPage={setPage}/>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-6 gap-2">
+          {movies.map((movie) => 
+          <div className='relative group overflow-hidden'>
+            <img key={movie.id}
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              className="rounded"
+            />
+            <div className='absolute inset-0 flex flex-col justify-center items-center backdrop-blur-md text-white
+                          opacity-0 group-hover:opacity-100 duration-300 rounded'>
+              <h2 className="font-bold text-lg leading-snug">{movie.title}</h2>
+              <p className='text-sm line-clamp-5'>{movie.overview}</p>
+            </div>
           </div>
+          )}
         </div>
-        )}
       </div>
-    </div>
+    </>
   );
 }
