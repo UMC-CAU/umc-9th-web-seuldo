635ec75ac8d876486665b6fefb7303f94a9c8ddd
anjaein <wodls9840@gmail.com>
2025-10-05 23:02:30 +0900

    navbar 컴포넌트 중복 제거

--- mission3/src/components/navbar.tsx
+++ mission3/src/components/navbar.tsx
@@ -1,33 +1,19 @@
 import { NavLink } from 'react-router-dom';
 
+const renderNavbar = (to : string, label :string) => (
+  <NavLink to={to} style={({ isActive }) =>
+    isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
+  }> {label} </NavLink>
+)
+
 const Navbar = () => {
   return (
     <nav className='flex justify-center gap-3 mb-6'>
-      <NavLink to="/" style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
-        }>
-        홈
-      </NavLink>
-            <NavLink to="/movies/popular" style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
-        }>
-        인기 영화
-      </NavLink>
-            <NavLink to="/movies/now_playing" style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
-        }>
-        상영 중
-      </NavLink>
-            <NavLink to="/movies/top_rated" style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
-        }>
-        평점 높은
-      </NavLink>
-            <NavLink to="/movies/upcoming" style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold', color: 'green' } : {color: 'gray'}
-        }>
-        개봉 예정
-      </NavLink>
+      {renderNavbar('/', '홈')}
+      {renderNavbar('/movies/popular', '인기 영화')}
+      {renderNavbar('/movies/now_playing', '상영 중')}
+      {renderNavbar('/movies/top_rated', '평점 높은')}
+      {renderNavbar('/movies/upcoming', '개봉 예정')}
     </nav>    
   );
 };
