808d39d9aa467977d95a8cc0085d8f6588b8ec14
anjaein <wodls9840@gmail.com>
2025-10-05 13:02:32 +0900

    reactrouter없이 spa 만들기: navbar 해결

--- spa-without-router/src/App.tsx
+++ spa-without-router/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react';
 
 const HomePage = () => <h1>슬도 페이지</h1>;
 const AboutPage = () => <p> 저는 UMC 9기 수강생 슬도입니다 </p>;
@@ -6,22 +7,46 @@ const ProjectsPage = () => <h1>프로젝트 페이지</h1>;
 const ContactPage = () => <p>https://github.com/UMC-CAU/umc-9th-web-seuldo</p>
 const NotFound = () => <h1>Not Found</h1>;
 
-function NavBar(){
+function NavBar({setPath}){
   return (
     <nav className='flex justify-center gap-5 border-b bg-gray-300 mb-5'>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/projects">Projects</a>
-      <a href="/contact">Contact</a>
+      <a href="/"
+        onClick={(e) => {
+        e.preventDefault()
+        window.history.pushState({}, '', '/')
+        setPath('/')
+        }}
+      >Home</a>
+      <a href="/about"
+        onClick={(e) => {
+        e.preventDefault()
+        window.history.pushState({}, '', '/about')
+        setPath('/about')
+        }}
+      >About</a>
+      <a href="/projects"
+        onClick={(e) => {
+        e.preventDefault()
+        window.history.pushState({}, '', '/projects')
+        setPath('/projects')
+        }}
+      >Projects</a>
+      <a href="/contact"
+        onClick={(e) => {
+        e.preventDefault()
+        window.history.pushState({}, '', '/contact')
+        setPath('/contact')
+        }}
+      >Contact</a>
     </nav>
   )
 }
 
 function App() {
-  const { pathname } = window.location;
+  const [path, setPath] = useState(window.location.pathname);
 
   let Page;
-  switch (pathname) {
+  switch (path) {
     case '/':
       Page = <HomePage />;
       break;
@@ -40,7 +65,7 @@ function App() {
 
   return (
     <>
-      <NavBar/>
+      <NavBar setPath={setPath}/>
       {Page}
     </>
   )
